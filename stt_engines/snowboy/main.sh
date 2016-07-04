25d5e0aa8c1754a13fc3996e5fce85ed619d7308
Alexandre Mély <alexandre.mely@gmail.com>
2016-07-04 22:27:27 +0200

    fix

--- stt_engines/snowboy/main.sh
+++ stt_engines/snowboy/main.sh
@@ -33,9 +33,10 @@
 }
 
 snowboy_STT () { # STT () {} Transcribes audio file $1 and writes corresponding text in $forder
+    shopt -s nocasematch
     $verbose && local quiet='' || local quiet='2>/dev/null'
     local model="snowboy.umdl"
-    [ $trigger != "snowboy" ] && model="$trigger.pmdl"
+    [ $trigger != "SNOWBOY" ] && model="$trigger.pmdl"
     eval python stt_engines/snowboy/main.py stt_engines/snowboy/resources/$model $quiet #WARNING:  140: This application, or a library it uses, is using the deprecated Carbon Component Manager for hosting Audio Units. Support for this will be removed in a future release. Also, this makes the host incompatible with version 3 audio units. Please transition to the API's in AudioComponent.h.
     if (( $? )); then
         echo "ERROR: snowboy recognition failed"
