abeef804df832f31dd614cb16128ce6527f73bcc
Alexandre Mély <alexandre.mely@gmail.com>
2016-08-01 18:46:17 +0200

    fixed #83 issue when several languages possible with osx

--- tts_engines/osx_say/main.sh
+++ tts_engines/osx_say/main.sh
@@ -13,6 +13,6 @@ hash 'say' 2>/dev/null || {
 }
 
 osx_say_TTS () { # TTS () {} Speaks text $1
-    voice=`/usr/bin/say -v ? | grep $language | awk '{print $1}'`
-    /usr/bin/say -v $voice "$1";
+    voice=`/usr/bin/say -v ? | grep $language | head -n 1 | awk '{print $1}'`
+    /usr/bin/say -v $voice "$1"
 }
