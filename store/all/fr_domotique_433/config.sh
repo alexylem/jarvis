b82356dcd5fe26627a90e72339c52ccbc4fb2369
MichelGard <raspberrypi.gard@gmail.com>
2016-09-27 23:02:11 +0200

    Mise en place module Domotique

---
+++ store/all/fr_domotique_433/config.sh
@@ -0,0 +1,6 @@
+#Exemple de variable "http://iplocal serveur arduino/?N°commande=valeur"
+#http://192.168.0.35/?LED2=ON
+#http://192.168.0.35/?LED2=OFF
+
+MG_lampe_salon_on="" 
+MG_lampe_salon_off="" 
