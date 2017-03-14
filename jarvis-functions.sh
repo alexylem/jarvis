e501161e5f64ea627d9ca4055c541b112e03c48d
Guillaume F <guillaume@eulerian.com>
2017-03-14 03:02:52 +0100

    snowboy integration for recording

--- jarvis-functions.sh
+++ jarvis-functions.sh
@@ -17,10 +17,10 @@ PLAY () { # PLAY () {} Play audio file $1
 }
 
 RECORD () { # RECORD () {} record microhphone to audio file $1 when sound is detected until silence
-    #$verbose && local quiet='' || local quiet='-q'
+    $verbose && local quiet='' || local quiet='-d'
     [ -n "$2" ] && local timeout="utils/timeout.sh $2" || local timeout=""
     [ $platform = "linux" ] && export AUDIODRIVER=alsa
-    local cmd="$timeout rec -V1 -q -r 16000 -c 1 -b 16 -e signed-integer --endian little $1 gain $gain silence 1 $min_noise_duration_to_start $min_noise_perc_to_start 1 $min_silence_duration_to_stop $min_silence_level_to_stop trim 0 $max_noise_duration_to_kill"
+    local cmd="$timeout python $quiet stt_engines/snowboy/mainwave.py $gain $1"
     $verbose && jv_debug "$cmd"
     eval $cmd # need eval because of timeout, maybe better to change this
     local retcode=$?
