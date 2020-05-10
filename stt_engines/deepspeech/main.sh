8874ef4b489841a58e5de990d4a5eaa5277b2d25
Philipp Leclercq <54592274+PhilLecl@users.noreply.github.com>
2020-05-10 12:51:16 +0200

    Add install and uninstall functionality for DeepSpeech

--- stt_engines/deepspeech/main.sh
+++ stt_engines/deepspeech/main.sh
@@ -1,13 +1,38 @@
 #!/bin/bash
 
+stt_deepspeech_install () {
+  set -e # Don't attempt to continue if an error occurs
+  jv_install python3 python3-pip virtualenv
+  cd "$(dirname "${BASH_SOURCE[0]}")"
+  virtualenv -p python3 .
+  source bin/activate
+  pip3 install deepspeech
+  deactivate
+  wget https://github.com/mozilla/DeepSpeech/releases/download/v0.7.0/deepspeech-0.7.0-models.pbmm
+  wget https://github.com/mozilla/DeepSpeech/releases/download/v0.7.0/deepspeech-0.7.0-models.scorer
+  cd "$jv_dir"
+}
+
+# Check if the needed files exist and install DeepSpeech if they don't
+[[ ( -f "$(dirname "${BASH_SOURCE[0]}")/deepspeech-0.7.0-models.pbmm" ) && \
+( -f "$(dirname "${BASH_SOURCE[0]}")/deepspeech-0.7.0-models.pbmm" ) && \
+( -d "$(dirname "${BASH_SOURCE[0]}")/bin" ) && \
+( -f "$(dirname "${BASH_SOURCE[0]}")/bin/deepspeech" ) ]] || {
+  dialog_yesno "DeepSpeech doesn't seem to be installed.\nDo you want to install it?" true >/dev/null && {
+      stt_deepspeech_install
+      dialog_msg "DeepSpeech installed sucessfully"
+  }
+}
+
 _deepspeech_transcribe () {
   cd "$(dirname "${BASH_SOURCE[0]}")"
-  source ./bin/activate
+  source bin/activate
   transcribed="$(deepspeech --model $deepspeech_model $([[ -n "$deepspeech_scorer" ]] \
-    && echo "--scorer $deepspeech_scorer ")--audio $audiofile 2>/dev/null)"
+    && echo "--scorer $deepspeech_scorer ")--audio $audiofile 2>"deepspeech.log")"
   deactivate
+  cd "$jv_dir"
 
-  $verbose && jv_debug "DEBUG: $transcribed" 
+  $verbose && jv_debug "DEBUG: $transcribed"
 
   echo $transcribed > $forder
 }
